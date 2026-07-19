843dd75f34d0349708ee326efcbd69cd1210a227
Anburoselin <anburosali@gmail.com>
2026-07-19 16:32:11 +0530

    Add files via upload
    Day1 activity

---
+++ Day1/classRoomActivityDatatypes.js
@@ -0,0 +1,15 @@
+
+//Day 1 Home assignment - Data types
+
+// Declaring variables using let
+let firstName = "Anbu";
+let companyName ="XYZ";
+let mobileNumber =9952090199;
+let isAutomation = true ;
+let hasPlaywright;
+// Printing values and their data types
+console.log("Variable name : firstName , value: " +firstName+ " and type of firstName: "  + typeof firstName)
+console.log("Variable name : companyName , value: " +companyName+ " and type of companyName: "  + typeof companyName)
+console.log("Variable name : mobileNumber , value: " +mobileNumber+ " and type of mobileNumber: "  + typeof mobileNumber)
+console.log("Variable name : isAutomation , value: " +isAutomation+ " and type of isAutomation: "  + typeof isAutomation)
+console.log("Variable name : hasPlaywright , value: " +hasPlaywright+ " and type of hasPlaywright: "  + typeof hasPlaywright)
